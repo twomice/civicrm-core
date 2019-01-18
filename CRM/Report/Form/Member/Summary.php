176541f8cded0c3a509c9d20fec09797834f2a33
Allen Shaw <allen@joineryhq.com>
2019-01-18 12:25:06 -0600

    Make 'amount statistics' optional
    Suggested by Stoob.

--- CRM/Report/Form/Member/Summary.php
+++ CRM/Report/Form/Member/Summary.php
@@ -159,7 +159,6 @@ class CRM_Report_Form_Member_Summary extends CRM_Report_Form {
           ),
           'total_amount' => array(
             'title' => ts('Amount Statistics'),
-            'required' => TRUE,
             'statistics' => array(
               'sum' => ts('Total Payments Made'),
               'count' => ts('Contribution Count'),
